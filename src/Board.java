0b232c7bca4010908c2a05483fe4af741ce55a01
Alexander van Doorn <turboblaster@live.nl>
2015-09-04 15:53:39 +0200

    bird & board changes

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -6,22 +10,108 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class Board extends JPanel{
+public class Board extends JPanel
+        implements Runnable {
 
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int INITIAL_X = -40;
+    private final int INITIAL_Y = -40;
+    private final int DELAY = 25;
+    
     private BufferedImage background;
 
+    private Image star;
+    private Thread animator;
+    private int x, y;
+
     public Board() {
-       try {                
-          background = ImageIO.read(new File("./src/images/background.png"));
-       } catch (IOException ex) {
-            // handle exception...       
-    	   }
+
+        initBoard();
+    }
+
+    private BufferedImage loadImage(String imagePath) {
+
+    	try {
+			return ImageIO.read(new File(imagePath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+    }
+
+    private void initBoard() {
+    	background = loadImage("./src/images/background.png");
+    	
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        setDoubleBuffered(true);
+
+//        loadImage();
+
+        x = INITIAL_X;
+        y = INITIAL_Y;
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(background, 0, -350, null); // see javadoc for more info on the parameters            
+    public void addNotify() {
+        super.addNotify();
+
+        animator = new Thread(this);
+        animator.start();
     }
 
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        drawImage(g, background);
+    }
+
+    private void drawImage(Graphics g, BufferedImage image) {
+
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void cycle() {
+
+        x += 1;
+        y += 1;
+
+        if (y > B_HEIGHT) {
+
+            y = INITIAL_Y;
+            x = INITIAL_X;
+        }
+    }
+
+    @Override
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while (true) {
+
+            cycle();
+            repaint();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted: " + e.getMessage());
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }
 }
