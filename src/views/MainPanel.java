1fadc751aa284d47e5bbfe1e737d6ef273efb8c8
jertje260 <jwbroekh@avans.nl>
2015-10-08 01:45:13 +0200

    score added

--- src/views/MainPanel.java
+++ src/views/MainPanel.java
@@ -9,18 +9,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 import containers.InputContainer;
+import factories.LevelFactory;
 import models.Unit;
 
 public class MainPanel extends JPanel {
     private BufferedImage background;
     private ArrayList<UnitView> unitViews;
     private HashMap<Unit, UnitView> views;
+    private JTextPane score;
+    private int points;
 
     public MainPanel() {
     	views = new HashMap<Unit, UnitView>();
@@ -40,7 +45,14 @@ public class MainPanel extends JPanel {
     }
 
     private void initBoard() {
+    	
+    	
     	this.background = loadImage("./src/images/background.png");
+    	
+    	score = new JTextPane();
+    	score.setEditable(false);
+    	score.setText("Score: 0" /* + LevelFactory.getInstance().currentLevel.Kills*/);
+    	this.add(score);
     	this.addMouseListener(new MouseListener(){
 
 			@Override
@@ -80,12 +92,15 @@ public class MainPanel extends JPanel {
     
     // call this method when a new unit is created.
     public void UpdateUnitViews(List<Unit> units){
+    	
     	for(Unit u : units){
     		boolean existing = false;
-    		for(UnitView v : unitViews){
-    			if(v.getUnit() == null){
+    		
+    		for(Iterator<UnitView> iterator = unitViews.iterator(); iterator.hasNext();){
+    			UnitView v = iterator.next();
+    			if(v.getUnit() == null){ //if(v.getUnit().getState() == died){
     				this.remove(v);
-    				unitViews.remove(v);
+    				iterator.remove();
     			}else {
 	    			if(v.getUnit().equals(u)){
 	    				existing = true;
@@ -96,16 +111,22 @@ public class MainPanel extends JPanel {
     			UnitView uv = new UnitView(u);
     			unitViews.add(uv);
     			this.add(uv);
-    			uv.repaint();
     			
     		}
     	}
-
+    	if(LevelFactory.getInstance().currentLevel != null){
+    		points = LevelFactory.getInstance().currentLevel.Kills;
+    	} else {
+    		points = 0;
+    	}
+    	
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        
+        score.setText("Score: " + points);
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
         for(UnitView v : unitViews){
         	v.draw(g);
